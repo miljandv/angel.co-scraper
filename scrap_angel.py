538decace132fcab6eea8ed893fca55b55e1193c
Miljan Djordjevic <miljandv@gmail.com>
2020-04-10 03:06:19 +0200

    Location scraping added

--- scrap_angel.py
+++ scrap_angel.py
@@ -18,13 +18,15 @@ driver.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfP
 time.sleep(30)
 # driver.quit()
 
+#python_button = driver.find_elements_by_xpath("//input[@name='lang' and @value='Python']")[0]
+#for i in range(20):
+#    python_button.click()
 
 
+results = driver.find_elements_by_xpath("//*[@class='main_container']//*[@class='base startup']//*[@class='company column']//*[@class='g-lockup']//*[@class='text']//*[@class='name']//*[@class='startup-link']")
+results_location = driver.find_elements_by_xpath("//*[@class='main_container']//*[@class='base startup']//*[@class='column location']//*[@class='value']//*[@class='tag']")
 
-#results = driver.find_elements_by_xpath("//*[@class='main_container']//*[@class='base startup']//*[@class='company column']//*[@class='g-lockup']//*[@class='text']//*[@class='name']//*[@class='startup-link']")
 
-
-results = driver.find_elements_by_xpath("//*[@class='main_container']//*[@class='base startup']//*[@class='company column']//*[@class='g-lockup']//*[@class='text']//*[@class='name']")
 print('Number of results', len(results))
 
 
@@ -33,11 +35,12 @@ print('Number of results', len(results))
 # create empty array to store data
 data = []
 # loop over results
-for result in results:    
-    product_name = result.text
+for i in range(len(results)):    
+    company_name = results[i].text
     #angel_link = result.find_element_by_tag_name('a')
-    angel_link = result.get_attribute("href")
-    data.append({"product" : product_name, "link" : angel_link})
+    angel_link = results[i].get_attribute("href")
+    location = results_location[i].text 
+    data.append({"Company name" : company_name, "Angel link" : angel_link, "Location" : location})
     
     
     
