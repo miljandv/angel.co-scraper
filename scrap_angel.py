eedea301b060b626e4dc7736c0669b7f985b6ebe
Miljan Djordjevic <miljandv@gmail.com>
2020-04-10 03:30:35 +0200

    Employees

--- scrap_angel.py
+++ scrap_angel.py
@@ -20,10 +20,12 @@ time.sleep(30)
 
 results = driver.find_elements_by_xpath("//*[@class='main_container']//*[@class='base startup']//*[@class='company column']//*[@class='g-lockup']//*[@class='text']//*[@class='name']//*[@class='startup-link']")
 results_location = driver.find_elements_by_xpath("//*[@class='main_container']//*[@class='base startup']//*[@class='column location']//*[@class='value']")
-results_websites = driver.find_elements_by_xpath("//*[@class='main_container']//*[@class='base startup']//*[@class='column website']//*[@class='value']")
+results_websites = driver.find_elements_by_xpath("//*[@class='main_container']//*[@class='base startup']//*[@class='column website']//*[@class='value']//*[@class='website']")
+results_employees = driver.find_elements_by_xpath("//*[@class='main_container']//*[@class='base startup']//*[@class='column company_size hidden_column']//*[@class='value']")
 
 print('Number of results', len(results))
 print('Number of results', len(results_location))
+print('Number of results', len(results_employees))
 
 
 
@@ -37,8 +39,9 @@ for i in range(len(results)):
     location = results_location[i]
     angel_link = results[i].get_attribute("href")
     location = location.text
-    website = results_websites[i]
-    data.append({"Company name" : company_name, "Angel link" : angel_link, "Location" : location, "Website" : website})
+    website = results_websites[i].text
+    employees = results_employees[i].text
+    data.append({"Company name" : company_name, "Angel link" : angel_link, "Location" : location, "Website" : website, "Number of employees" : employees})
     
     
     
