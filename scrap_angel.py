c1320a9d75e7a8dde64d9e0b433893fded06515e
Miljan Djordjevic <miljandv@gmail.com>
2020-04-10 03:20:57 +0200

    Website scraping added

--- scrap_angel.py
+++ scrap_angel.py
@@ -4,30 +4,26 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
 import pandas as pd
-# specify the url
 urlpage = 'https://angel.co/companies' 
 print(urlpage)
-# run firefox webdriver from executable path of your choice
 driver = webdriver.Firefox(executable_path = 'geckodriver')
 
-# get web page
 driver.get(urlpage)
-# execute script to scroll down the page
 driver.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
-# sleep for 30s
 time.sleep(30)
-# driver.quit()
 
 #python_button = driver.find_elements_by_xpath("//input[@name='lang' and @value='Python']")[0]
 #for i in range(20):
 #    python_button.click()
 
 
-results = driver.find_elements_by_xpath("//*[@class='main_container']//*[@class='base startup']//*[@class='company column']//*[@class='g-lockup']//*[@class='text']//*[@class='name']//*[@class='startup-link']")
-results_location = driver.find_elements_by_xpath("//*[@class='main_container']//*[@class='base startup']//*[@class='column location']//*[@class='value']//*[@class='tag']")
 
+results = driver.find_elements_by_xpath("//*[@class='main_container']//*[@class='base startup']//*[@class='company column']//*[@class='g-lockup']//*[@class='text']//*[@class='name']//*[@class='startup-link']")
+results_location = driver.find_elements_by_xpath("//*[@class='main_container']//*[@class='base startup']//*[@class='column location']//*[@class='value']")
+results_websites = driver.find_elements_by_xpath("//*[@class='main_container']//*[@class='base startup']//*[@class='column website']//*[@class='value']")
 
 print('Number of results', len(results))
+print('Number of results', len(results_location))
 
 
 
@@ -36,11 +32,13 @@ print('Number of results', len(results))
 data = []
 # loop over results
 for i in range(len(results)):    
+    print(i)
     company_name = results[i].text
-    #angel_link = result.find_element_by_tag_name('a')
+    location = results_location[i]
     angel_link = results[i].get_attribute("href")
-    location = results_location[i].text 
-    data.append({"Company name" : company_name, "Angel link" : angel_link, "Location" : location})
+    location = location.text
+    website = results_websites[i]
+    data.append({"Company name" : company_name, "Angel link" : angel_link, "Location" : location, "Website" : website})
     
     
     
