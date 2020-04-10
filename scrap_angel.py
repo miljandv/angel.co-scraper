c84860a363cfacd9e3b1e745f37e818f3660ed47
Miljan Djordjevic <miljandv@gmail.com>
2020-04-10 03:41:39 +0200

    Raised?

--- scrap_angel.py
+++ scrap_angel.py
@@ -1,4 +1,3 @@
-# import libraries
 import urllib.request
 from bs4 import BeautifulSoup
 from selenium import webdriver
@@ -21,18 +20,19 @@ time.sleep(30)
 results = driver.find_elements_by_xpath("//*[@class='main_container']//*[@class='base startup']//*[@class='company column']//*[@class='g-lockup']//*[@class='text']//*[@class='name']//*[@class='startup-link']")
 results_location = driver.find_elements_by_xpath("//*[@class='main_container']//*[@class='base startup']//*[@class='column location']//*[@class='value']")
 results_websites = driver.find_elements_by_xpath("//*[@class='main_container']//*[@class='base startup']//*[@class='column website']//*[@class='value']//*[@class='website']")
-results_employees = driver.find_elements_by_xpath("//*[@class='main_container']//*[@class='base startup']//*[@class='column company_size hidden_column']//*[@class='value']")
+results_employees = driver.find_elements_by_xpath("//*[@class='main_container']//*[@class='base startup']//*[@class='column company_size']//*[@class='value']")
+#result_raised = driver.find_elements_by_xpath("//*[@class='main_container']//*[@class='base startup']//*[@class='column raised hidden_column']//*[@class='value']")
+
 
 print('Number of results', len(results))
 print('Number of results', len(results_location))
 print('Number of results', len(results_employees))
+#print('Number of results', len(result_raised))
 
 
 
 
-# create empty array to store data
 data = []
-# loop over results
 for i in range(len(results)):    
     print(i)
     company_name = results[i].text
@@ -41,8 +41,8 @@ for i in range(len(results)):
     location = location.text
     website = results_websites[i].text
     employees = results_employees[i].text
-    data.append({"Company name" : company_name, "Angel link" : angel_link, "Location" : location, "Website" : website, "Number of employees" : employees})
-    
+    #raised = result_raised[i].text
+    data.append({"Company name" : company_name, "Angel link" : angel_link, "Location" : location, "Website" : website, "Number of employees" : employees})#, "Raised" : raised
     
     
 driver.quit()
